2d48f3862a5d17617a2d702fe0eefc060b20e8b1
Jérôme Breton <jerome.breton@gmail.com>
2017-11-07 08:19:06 +0100

    Wrong param

--- svfs-mount.sh
+++ svfs-mount.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-svfs mount --debug --default-uid ${UID:-1000} --default-gid ${GID:-1000}  --default-mode ${MODE:-0755} --connect_timeout=${CONNECT_TIMEOUT:-15s} --segment_size=${SEGMENT_SIZE:-256} --allow-other --device hubic --mountpoint /ovh
+svfs mount --debug --default-uid ${UID:-1000} --default-gid ${GID:-1000}  --default-mode ${MODE:-0755} --os-connect-timeout=${CONNECT_TIMEOUT:-15s} --os-segment-size=${SEGMENT_SIZE:-256} --allow-other --device hubic --mountpoint /ovh
